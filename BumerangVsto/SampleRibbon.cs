f8142fcd35d0ee6a5c8c2de4a28c637e85552fc9
Vorobushek221 <dima-anisko@yandex.ru>
2017-01-23 13:16:43 +0300

    Hello World for addin

---
+++ BumerangVsto/SampleRibbon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using System.Windows.Forms;
+
+namespace BumerangVsto
+{
+    public partial class SampleRibbon
+    {
+        public event Action ButtonClicked;
+
+        private void SampleRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            if (ButtonClicked != null)
+                ButtonClicked();
+        }
+    }
+}
--- BumerangVsto/ThisAddIn.cs
+++ BumerangVsto/ThisAddIn.cs
@@ -6,6 +6,8 @@ using System.Xml.Linq;
 using Excel = Microsoft.Office.Interop.Excel;
 using Office = Microsoft.Office.Core;
 using Microsoft.Office.Tools.Excel;
+using System.Windows.Forms;
+using Microsoft.Office.Tools.Ribbon;
 
 namespace BumerangVsto
 {
@@ -18,6 +20,71 @@ namespace BumerangVsto
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
         {
         }
+
+        protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+        {
+            var ribbon = new SampleRibbon();
+            ribbon.ButtonClicked += ribbon_ButtonClicked;
+            return Globals.Factory.GetRibbonFactory().CreateRibbonManager(new IRibbonExtension[] { ribbon });
+        }
+
+        private void ribbon_ButtonClicked()
+        {
+            GenerateTable(Application.ActiveWorkbook.ActiveSheet, 1, 1);
+        }
+
+        private void GenerateTable(dynamic wSheet, int row, int column)
+        {
+            var currentMonth = GetDaysOfCurrentMonth().ToArray();
+            for (int i = 0; i < currentMonth.Length; i++)
+            {
+                wSheet.Cells[row, column + i] = currentMonth[i].Day;
+                MarkCell(wSheet.Cells[row, column + i], currentMonth[i]);
+            }
+        }
+
+        public static void MarkBold(dynamic border)
+        {
+            border.Weight = Office.XlBorderWeight.xlMedium;
+        }
+
+        public enum Border
+        {
+            Left = 1,
+            Right = 2,
+            Top = 3,
+            Bottom = 4
+        }
+
+        private void MarkCell(dynamic cell, DateTime day)
+        {
+            if (day.DayOfWeek == DayOfWeek.Saturday)
+            {
+                MarkBold(cell.Borders[Border.Left]);
+                MarkBold(cell.Borders[Border.Top]);
+                MarkBold(cell.Borders[Border.Bottom]);
+            }
+            if (day.DayOfWeek == DayOfWeek.Sunday)
+            {
+                MarkBold(cell.Borders[Border.Right]);
+                MarkBold(cell.Borders[Border.Top]);
+                MarkBold(cell.Borders[Border.Bottom]);
+            }
+            cell.Columns[1].ColumnWidth = 4;
+        }
+
+        private static IEnumerable<DateTime> GetDaysOfCurrentMonth()
+        {
+            var today = DateTime.Today;
+            var dayIter = new DateTime(today.Year, today.Month, 1);
+            while (dayIter.Month == today.Month)
+            {
+                yield return dayIter;
+                dayIter = dayIter.AddDays(1);
+            }
+        }
+
+
 
         #region Код, автоматически созданный VSTO
 
