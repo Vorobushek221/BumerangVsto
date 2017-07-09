7c01330355ed21e4e21703d87e96aff49872d10d
Анисько Дмитрий Геннадьевич <vorobushek221@gmail.com>
2017-07-09 23:06:02 +0300

    Replaced TemplateType.cs file.

---
+++ BumerangVsto/Model/TemplateType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BumerangVsto.Business.Model
+{
+    public enum TemplateType
+    {
+        Tags2,
+        Tags3,
+        Tags5
+    }
+}
